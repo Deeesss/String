7fc2962083f4baa1e44548f73375983f0905b14c
Deeesss <111076028+Deeesss@users.noreply.github.com>
2023-09-18 10:33:07 +0200

    Strings
    Lessons

---
+++ Jednoduchy prikladt strings.py
@@ -0,0 +1,9 @@
+print("Zadej jméno")
+jmeno = str.upper(input())
+print("Zadej příjmení")
+příjmení = str.upper(input())
+print("Zadej svůj věk")
+věk = int(input())
+print(f"{jmeno} {příjmení} ")
+print(f"Za rok ti bude {věk + 1}")
+
